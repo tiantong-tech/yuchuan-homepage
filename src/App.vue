b839873524807dc448593fa2ad7c4d0c1ec53005
delylaric <delylaric@gmail.com>
2019-05-22 09:49:52 +0800

    页脚

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
     <Level></Level>
-    <router-view/>
+    <!-- 路由包装，解决 SPA 页面未加载就直接显示页脚的问题 -->
+    <div style="min-height: calc(100vh - 7rem)">
+      <router-view />
+    </div>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Level from '@/components/Level'
+import Footer from '@/components/AppFooter'
 
 export default {
   name: 'App',
   components: {
-    Level
+    Level,
+    Footer
   }
 }
 </script>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="app-footer has-background-link">
+    <div class="content has-text-centered has-text-white">
+      <p>客服热线: +86-21-6105-7311 &nbsp;&nbsp; QQ: 150287070</p>
+      <p>E-Mail：michellezhang@als-yuchuan.com</p>
+      <p>地址：上海市奉贤区解放东路1018弄1619</p>
+      <p>© 2019 www.als-yuchuan.com 沪ICP备16024334号</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'HomeFooter'
+}
+</script>
+
+<style lang="sass">
+.app-footer
+  padding-top: 4rem
+  padding-bottom: 4rem
+</style>
