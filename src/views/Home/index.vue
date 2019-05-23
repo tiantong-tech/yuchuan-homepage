b122bdaf78b5bb50f8ff1ef8ca9701cc95ba4af7
delylaric <delylaric@gmail.com>
2019-05-23 08:33:55 +0800

    优化首页字体大小

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -12,20 +12,20 @@
       </div>
     </Hero>
     <Hero
-      v-for="key in 10" :key="key"
+      v-for="key in 5" :key="key"
       :class="'has-background-' + (key % 2 ? 'white' : 'white-ter')"
     >
       <div>
-        <h1 class="title is-2 has-text-black" style="margin-bottom: 2.5rem">
+        <h2 class="title is-3 has-text-black" style="margin-bottom: 2.5rem">
           ERP/WMS软件系统
-        </h1>
-        <h2 class="subtitle is-4 has-text-black">
+        </h2>
+        <p class="subtitle is-5 has-text-black">
           WMS软件系统，是一款仓储管理软件产品，
           旨在通过入库业务、出库业务、仓库调拨、库存调拨等功能，
           综合实现批次管理、物料对应、库存盘点、质检管理等功能的管理系统，
           它能有效控制并跟踪仓库业务的物流和成本管理全过程，实现完善的企业仓储信息管理。
           该软件既可以独立执行库存管理，也可与ERP、TMS等系统结合使用，可提供完整全面的企业业务流程和管理信息。
-        </h2>
+        </p>
         <a class="button is-link is-right">详细介绍 →</a>
       </div>
       <div class="is-flex-auto" style="min-width: 3rem"></div>
