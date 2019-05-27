bd93523092262c77649d20187bec850ea15a5c5f
delylaric <delylaric@gmail.com>
2019-05-27 16:52:04 +0800

    首页内容增加

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,79 +1,71 @@
 <template>
   <div id="app-index">
-    <Hero class="has-background-white-ter" image="index-hero-1.jpg" color="white" height="744px">
+    <Hero color="white" image="index-hero-1.jpg" height="744px">
       <div style="width: 560px">
-        <h1 class="title is-2 has-text-white-ter" style="margin-bottom: 2.5rem">
+        <h1 class="is-size-2 has-text-white" style="margin-bottom: 1rem">
           智能物流专家
         </h1>
-        <p class="subtitle is-4 has-text-white-ter">
+        <p class="is-size-4 has-text-white">
           智能物流是利用集成智能化技术，使物流系统能模仿人类的智能，
           具有思维、感知、学习、推理判断和自行解决物流中某些问题的能力
         </p>
       </div>
     </Hero>
-    <Hero
-      v-for="key in 5" :key="key"
-      :class="'has-background-' + (key % 2 ? 'white' : 'white-ter')"
-    >
+    <Hero color="white-ter">
       <div>
-        <h2 class="title is-3 has-text-black" style="margin-bottom: 2.5rem">
-          ERP/WMS软件系统
+        <h2 class="is-size-3" style="margin-bottom: 1rem">
+          垂直物流输送系统
         </h2>
-        <p class="subtitle is-5 has-text-black">
-          WMS软件系统，是一款仓储管理软件产品，
-          旨在通过入库业务、出库业务、仓库调拨、库存调拨等功能，
-          综合实现批次管理、物料对应、库存盘点、质检管理等功能的管理系统，
-          它能有效控制并跟踪仓库业务的物流和成本管理全过程，实现完善的企业仓储信息管理。
-          该软件既可以独立执行库存管理，也可与ERP、TMS等系统结合使用，可提供完整全面的企业业务流程和管理信息。
+        <p class="is-size-5">
+          垂直物流输送是在指多层建筑结构中，用各种输送设备进行作业，
+          信息以及控制的组合实现货物水平和垂直自由搬运的一种自动非载人精密设备。
         </p>
-        <a class="button is-link is-right">详细介绍 →</a>
       </div>
       <div class="is-flex-auto" style="min-width: 3rem"></div>
-      <VImage path="index-wms-pic.png"></VImage>
+      <VImage style="width: 512px; height: 392px" path="index-hoister-2.jpg"></VImage>
     </Hero>
-    <!-- <Hero image="index-hero-2.png">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title is-2" style="margin-bottom: 2.5rem">
-            垂直物流输送系统
-          </h1>
-          <h2 class="subtitle is-4">
-            垂直物流输送是在指多层建筑结构中，用各种输送设备进行作业，
-            信息以及控制的组合实现货物水平和垂直自由搬运的一种自动非载人精密设备
-          </h2>
-          <VideoButton></VideoButton>
-        </div>
+    <Hero color="white">
+      <div>
+        <h2 class="is-size-3" style="margin-bottom: 1rem">
+          物流设备
+        </h2>
+        <p class="is-size-5">
+          自动化物流设备是在无人干预的情况下，根据已经设定的指令或者程序，
+          自动完成工作流程的任务，在各行业的生产制造、物流运输都起着重要作用。
+        </p>
       </div>
-    </Hero> -->
-    <!-- <Hero image="index-hero-3.jpg">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title is-2" style="margin-bottom: 2.5rem">
-            自动化立体仓库系统
-          </h1>
-          <h2 class="subtitle is-4">
-            自动化立体仓库是采用高层货架配以料箱或托盘储存货物，
-            用巷道堆垛起重机及其他输送设备在信息WMS以及设备控制WCS的支配下进行联动作业，
-            以实现仓储物流合理化，存取自动化，操作简便化的自动化物流仓储系统
-          </h2>
-          <VideoButton></VideoButton>
-        </div>
+      <div class="is-flex-auto" style="min-width: 3rem"></div>
+      <VImage style="width: 880px; height: 392px" path="index-device-1.png"></VImage>
+    </Hero>
+    <Hero color="white-ter">
+      <div>
+        <h2 class="is-size-3" style="margin-bottom: 1rem">
+          自动化立体仓库系统
+        </h2>
+        <p class="is-size-5">
+          自动化立体仓库是采用高层货架配以料箱或托盘储存货物，
+          用巷道堆垛起重机及其他输送设备在信息WMS以及设备控制WCS的支配下进行联动作业，
+          以实现仓储物流合理化，存取自动化，操作简便化的自动化物流仓储系统。
+        </p>
       </div>
-    </Hero> -->
-    <!-- <Hero image="index-hero-2.png">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title is-2" style="margin-bottom: 2.5rem">
-            物流设备
-          </h1>
-          <h2 class="subtitle is-4">
-            垂直物流输送是在指多层建筑结构中，用各种输送设备进行作业，
-            信息以及控制的组合实现货物水平和垂直自由搬运的一种自动非载人精密设备
-          </h2>
-          <VideoButton></VideoButton>
-        </div>
-      </div>
-    </Hero> -->
+      <div class="is-flex-auto" style="min-width: 3rem"></div>
+      <VImage style="width: 512px; height: 392px" path="index-liku-1.jpg"></VImage>
+    </Hero>
+    <div
+      class="is-flex"
+      style="justify-content: center; align-items: center; margin-top: 48px"
+    >
+      <h2 class="is-size-3">
+        WMS & WCS软件系统
+      </h2>
+    </div>
+    <div
+      class="container is-flex"
+      style="justify-content: center; align-items: center"
+    >
+      <VImage path="index-wms-1.png"></VImage>
+    </div>
+    <div style="height: 32px"></div>
   </div>
 </template>
 
