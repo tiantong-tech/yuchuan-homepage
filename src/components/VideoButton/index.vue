cc11c12254011919d3fbb17bc903793eb88d192a
zhanglan <zhanglan1315@gmail.com>
2019-07-22 21:57:06 +0800

    improve(video-button): 修复bug并增加缓存功能

--- src/components/VideoButton/index.vue
+++ src/components/VideoButton/index.vue
@@ -14,17 +14,19 @@
       :class="isShow && 'is-active'"
     >
       <div
-        @click="isShow = false"
+        @click="handleClose"
         class="modal-background"
       ></div>
       <div class="modal-content">
         <video
+          v-if="isShow || isCaching"
           id="yuchuan-introduce"
           class="video-js vjs-default-skin vjs-big-play-centered"
           style="border-bottom: none"
           autoplay="muted"
           preload="auto"
           controls
+          ref="video"
         >
           <slot></slot>
         </video>
@@ -34,18 +36,42 @@
 </template>
 
 <script>
+let timeout = null
+
 export default {
   name: 'VideoButton',
   props: {
     text: {
-      default: '播放视频'
+      default: '观看视频'
+    },
+    // 缓存时间，单位 s
+    cacheTime: {
+      default: 15
     }
   },
   data: () => ({
     isShow: false,
-    options: {
+    isCaching: false
+  }),
+  methods: {
+    handleOpen () {
+      this.isShow = true
+    },
+    handleClose () {
+      // 处理未结束的缓存
+      if (this.isCaching) {
+        clearTimeout(timeout)
+      }
 
+      timeout = setTimeout(
+        () => this.isCaching = false,
+        this.cacheTime * 1000
+      )
+
+      this.isCaching = true
+      this.isShow = false
+      this.$refs.video.pause()
     }
-  })
+  }
 }
 </script>
