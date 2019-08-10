c3ef475856937a2a825493a1b9e3eda2dc14662b
zhanglan <zhanglan1315@gmail.com>
2019-08-10 12:17:36 +0800

    improve(home): 调整首页行间距

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -5,14 +5,16 @@
       style="height: 600px"
     >
       <div style="width: 560px">
-        <h1 class="is-size-3 has-text-white" style="margin-bottom: 1rem">
+        <h1 class="is-size-3 has-text-white">
           智能物流专家
         </h1>
+        <div v-style:height="height"></div>
         <p class="has-text-white">
           智能物流是利用集成智能化技术，使物流系统能模仿人类的智能，
           具有思维、感知、学习、推理判断和自行解决物流中某些问题的能力
         </p>
-        <p style="margin-top: 1rem">
+        <div v-style:height="height"></div>
+        <p>
           <VideoButton>
             <source src="https://img.als-yuchuan.com/videos/yuchuan-introduce" type="video/mp4" >
           </VideoButton>
@@ -53,9 +55,10 @@
       >
         <div class="columns is-vcentered">
           <div class="column">
-            <h2 class="is-size-3" style="margin-bottom: 1rem">
+            <h2 class="is-size-3">
               垂直物流输送系统
             </h2>
+            <div v-style:height="height"></div>
             <p>
               垂直物流输送是在指多层建筑结构中，用各种输送设备进行作业，
               信息以及控制的组合实现货物水平和垂直自由搬运的一种自动非载人精密设备。
@@ -85,15 +88,17 @@
       >
         <div class="columns is-vcentered">
           <div class="column">
-            <h2 class="is-size-3" style="margin-bottom: 1rem">
+            <h2 class="is-size-3">
               自动化立体仓库系统
             </h2>
+            <div v-style:height="height"></div>
             <p>
               自动化立体仓库是采用高层货架配以料箱或托盘储存货物，
               用巷道堆垛起重机及其他输送设备在信息WMS以及设备控制WCS的支配下进行联动作业，
               以实现仓储物流合理化，存取自动化，操作简便化的自动化物流仓储系统。
             </p>
-            <div @click="handleJump" style="margin-top: 1rem; width: 100px">
+            <div v-style:height="height"></div>
+            <div @click="handleJump" style="width: 100px">
               <router-link to="/products/asrs">
                 了解更多 >
               </router-link>
@@ -114,20 +119,22 @@
       >
         <div class="columns is-vcentered">
           <div class="column">
-            <h2 class="is-size-3" style="margin-bottom: 1rem">
+            <h2 class="is-size-3">
               物流设备
             </h2>
+            <div v-style:height="height"></div>
             <p>
               自动化物流设备是在无人干预的情况下，根据已经设定的指令或者程序，
               自动完成工作流程的任务，在各行业的生产制造、物流运输都起着重要作用。
             </p>
-            <div @click="handleJump" style="margin-top: 1rem; width: 100px">
+            <div v-style:height="height"></div>
+            <div @click="handleJump" style="width: 100px">
               <router-link to="/products/rgv">
                 了解更多 >
               </router-link>
             </div>
           </div>
-          <div style="width: 2rem; height: 1rem"></div>
+          <div style="width: 2rem" v-style:height="height"></div>
           <div class="column">
             <figure class="image">
               <VImage path="home/device-1.png"></VImage>
@@ -169,6 +176,7 @@ export default {
     VideoButton
   },
   data: () => ({
+    height: '0.75rem',
     partners: [
       ['招商局', '格力', '武汉万吨'],
       ['中外运', '世纪联华', '鑫荣懋']
